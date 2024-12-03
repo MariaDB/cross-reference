d795fa2064a7df94675f471ca60e7661c1d85b48
Vlad Radu <raduvlad2006@gmail.com>
2024-12-03 17:15:52 +0200

    MDBF-680 Create Docker Image for Cross Reference tool
    Based on the previous PR for this MDBF (https://github.com/MariaDB/buildbot/pull/412), a dedicated repository was created for Cross Reference.
    
    This is a port from the work done by Andreia Hendea, Daniel Black, Faustin Lammler and Vlad Bogolin.

---
+++ src/cr/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from cr.models import TestFailure, TestRun
+
+admin.site.register(TestFailure)
+admin.site.register(TestRun)
+# Register your models here.
---
+++ src/crossreference/settings.py
@@ -0,0 +1,92 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(os.environ['DJANGO_DEBUG'])
+
+# ALLOWED_HOSTS
+ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOSTS']]
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'cr',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'crossreference.urls'
+WSGI_APPLICATION = 'crossreference.wsgi.application'
+ASGI_APPLICATION = 'crossreference.asgi.application'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DJANGO_DB_NAME'],
+        'USER': os.environ['DJANGO_DB_USER_NAME'],
+        'PASSWORD': os.environ['DJANGO_DB_USER_PASSWORD'],
+        'HOST': os.environ['DJANGO_DB_HOST'],
+        'PORT': os.environ['DJANGO_DB_PORT'],
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/cr/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
